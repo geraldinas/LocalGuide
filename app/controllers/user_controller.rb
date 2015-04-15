e9ed1df93f797e15b1dbd8313654c7572790e77d
danilaurenhill <dani.lauren.hill@gmail.com>
2015-04-15 13:05:45 -0400

    added a profile page w some html

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def profile
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: 'static_pages#index'
+
+  get "/profile" => "user#profile"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   get "/auth/twitter/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
+
+  resources :user, :only => [:show, :index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
