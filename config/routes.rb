684623c840e005b041a83c3852b7ea02487c033b
Stephanie Aviles <avilestephcode@gmail.com>
2015-04-16 09:19:06 -0400

    facebook omni

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 
   get "/profile" => "users#profile"
 
-<<<<<<< HEAD
+
   get "/auth/facebook/callback" => "sessions#create"
-=======
   get "/trip" => "users#trip"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
@@ -20,8 +19,6 @@ Rails.application.routes.draw do
 
   # get "/profile" => "users#profile"
 
-  get "/auth/twitter/callback" => "sessions#create"
->>>>>>> c51403463010a6b64c91ab3addaae2b16a215254
   get "/signout" => "sessions#destroy", :as => :signout
 
   resources :users, :only => [:show, :index]
