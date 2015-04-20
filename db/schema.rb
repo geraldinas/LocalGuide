d48a8ffc578f6077f155a3e6d6bcaa22e35332bc
geraldinas <geraldinagalvarez@gmail.com>
2015-04-20 10:44:17 -0400

    added column to Tours

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416150536) do
+ActiveRecord::Schema.define(version: 20150420144107) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20150416150536) do
     t.integer  "activity_id"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "status",      default: "pending"
   end
 
   create_table "trips", force: :cascade do |t|
