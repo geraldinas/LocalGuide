fae956f5b61b9a27a3e24d93a76349501cec3cd0
danilaurenhill <dani.lauren.hill@gmail.com>
2015-04-22 16:58:08 -0400

    added the name to each location pin as a pop-up

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -27,8 +27,9 @@ class ActivitiesController < ApplicationController
     @location_json = Gmaps4rails.build_markers(locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
+      marker.infowindow location.name
       marker.json ({
-        :title => "#{location.name}"
+        
       })
     end
 	end
