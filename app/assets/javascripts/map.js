f54363f3c282a6d0296654004d5e4e9cdd726ff7
danilaurenhill <dani.lauren.hill@gmail.com>
2015-04-22 14:37:26 -0400

    created a scrollable map plotting multiple locations

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,10 @@
+$(function() {
+  if ($("#map").length > 0) {
+    handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+      markers = handler.addMarkers(rawMarkers);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+    });
+  }
+});
