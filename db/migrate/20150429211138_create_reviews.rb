109a54e75a5a75ffaf089fccad7a80865fd018b8
Stephanie Aviles <avilestephcode@gmail.com>
2015-04-29 17:41:53 -0400

    reviews and stuff

---
+++ db/migrate/20150429211138_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+    	t.integer  :guide_id
+    	t.integer  :reviewer_id
+    	t.text     :review 
+      t.timestamps null: false
+    end
+  end
+end
