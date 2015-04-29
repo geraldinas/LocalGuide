2cc9bf56ed267a2bf8d449117260e9315244bb09
Stephanie Aviles <avilestephcode@gmail.com>
2015-04-29 18:21:19 -0400

    fixed migration error

--- db/migrate/20150429211138_create_reviews.rb
+++ db/migrate/20150429211138_create_reviews.rb
@@ -3,11 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
     	t.integer  :guide_id
     	t.integer  :reviewer_id
-<<<<<<< HEAD
-    	t.text     :review 
-=======
     	t.text     :content
->>>>>>> review
       t.timestamps null: false
     end
   end
